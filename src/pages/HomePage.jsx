a2177c223464398de904d2ea16d4a05531430bd4
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-02-07 15:51:24 +0530

    testing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,6 +20,7 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { cart, setCart } = useCart();
+  const apiUrl = 'https://ecommercebackend-6w18.onrender.com';
 
   //get total
   const getTotal = async () => {
@@ -54,7 +55,7 @@ const HomePage = () => {
   const getAllCategories = async () => {
     try {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_API}/api/v1/category/getall-categories`
+        `${apiUrl}/api/v1/category/getall-categories`
       );
 
       if (data.success) {
