8f567521e1a4d187a82f9307a9cfd0f2124b71f8
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-02-07 15:36:58 +0530

    feat-changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://ecommercebackend-6w18.onrender.com', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    // No need for proxy configuration
   },
+  define: {
+    'process.env': process.env
+  }
 });
